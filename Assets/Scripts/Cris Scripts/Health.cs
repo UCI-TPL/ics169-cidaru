9a0d5fd0fa82710ffb5f81955f5a8770da841bc2
Crystal <crystal.agerton2015@gmail.com>
2018-10-18 21:15:52 -0700

    Added Health

---
+++ Assets/Scripts/Cris Scripts/Health.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+    /// Tracks the health of the object
+    /// Based on:
+    /// https://unity3d.com/learn/tutorials/projects/survival-shooter/player-health
+
+    public int startingHealth = 10;
+    public int currentHealth;
+
+    private bool isDead; //TODO: Make private
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+        isDead = false;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+
+        if (currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+    public void HealBy(int amount)
+    {
+        if (currentHealth + amount >= startingHealth)
+            currentHealth = startingHealth;
+        else
+            currentHealth += amount;
+    }
+
+    private void Death()
+    {
+        isDead = true;
+    }
+
+    public bool dead()
+    {
+        return isDead;
+    }
+
+    public void Reset()
+    {
+        currentHealth = startingHealth;
+        isDead = false;
+    }
+}
--- Assets/Scripts/Cris Scripts/PlayerController.cs
+++ Assets/Scripts/Cris Scripts/PlayerController.cs
@@ -1,24 +1,42 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Health))]
 public class PlayerController : MonoBehaviour
 {
-    /// Controls player's movement
-    /// Based on:
-    /// https://unity3d.com/learn/tutorials/projects/survival-shooter/player-character?playlist=17144
-    ///
+    /* Controls player
+    Based on:
+    https://unity3d.com/learn/tutorials/projects/survival-shooter/player-character?playlist=17144
+    */
 
     public float speed = 5f;
 
+    private Health health;
+
+    private void Start()
+    {
+        health = GetComponent<Health>();
+    }
+
     void FixedUpdate()
     {
+        if (!health.dead())
+            Move();
+
+        #region Reset Health (When Dead)
+        else if (Input.GetKey(KeyCode.Space))
+        {
+            health.Reset();
+        }
+        #endregion
+    }
+
+    void Move()
+    {
+        /* Basic free movement */
+
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
 
-        Move(x, y);
-    }
-
-    void Move(float x, float y)
-    {
         Vector3 movement = new Vector3(x, y, 0);
         movement = movement.normalized * speed * Time.deltaTime;
         gameObject.transform.Translate(movement);

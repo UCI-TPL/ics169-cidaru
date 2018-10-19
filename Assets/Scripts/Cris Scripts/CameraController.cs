662b94c07bc969ad0da6bf6a92487f91f142410d
Crystal <crystal.agerton2015@gmail.com>
2018-10-18 20:07:01 -0700

    Initial Movement and Camera

---
+++ Assets/Scripts/Cris Scripts/CameraController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    public MoveType moveType;
+
+    private Vector3 offset;
+    public float smoothTime = 0.125f;
+
+    private Vector3 velocity;
+
+    public enum MoveType
+    {
+        basicFollow, smoothDampFollow, LerpFollow
+    }
+
+    void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	void LateUpdate () {
+        switch (moveType)
+        {
+            case MoveType.smoothDampFollow:
+                smoothDampFollow();
+                break;
+            case MoveType.LerpFollow:
+                LerpFollow();
+                break;
+            default:
+                basicFollow();
+                break;
+        }
+	}
+
+    void basicFollow()
+    {
+        transform.position = player.transform.position + offset;
+    }
+
+    void smoothDampFollow()
+    {
+        Vector3 newPosition = player.transform.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
+    }
+
+    void LerpFollow()
+    {
+        Vector3 newPosition = player.transform.position + offset;
+        transform.position = Vector3.Lerp(transform.position, newPosition, smoothTime);
+    }
+}
---
+++ Assets/Scripts/Cris Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    /// Controls player's movement
+    /// Based on:
+    /// https://unity3d.com/learn/tutorials/projects/survival-shooter/player-character?playlist=17144
+    ///
+
+    public float speed = 5f;
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        Move(x, y);
+    }
+
+    void Move(float x, float y)
+    {
+        Vector3 movement = new Vector3(x, y, 0);
+        movement = movement.normalized * speed * Time.deltaTime;
+        gameObject.transform.Translate(movement);
+    }
+}

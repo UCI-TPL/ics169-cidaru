393fcffbf8a970afc79a98edb087d6d6d0ea4c89
Crystal <crystal.agerton2015@gmail.com>
2018-10-21 14:31:22 -0700

    Time Manipulation

---
+++ Assets/Scripts/Cris Scripts/TimeAreaEffector.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeAreaEffector : MonoBehaviour {
+
+    public float timeEffect = 1f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ChangeTime(collision.gameObject, timeEffect);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        ChangeTime(collision.gameObject, 1/timeEffect);
+    }
+
+    private void ChangeTime(GameObject other, float effect)
+    {
+        if (other.tag.Equals("Player"))
+            changePlayerTime(other, effect);
+        else if (other.tag.Equals("Enemy"))
+            changeEnemyTime(other, effect);
+        else if (other.tag.Contains("Bullet"))
+            changeBulletTime(other, effect);
+    }
+
+    private void changePlayerTime(GameObject player, float effect)
+    {
+        PlayerController pc = player.GetComponent<PlayerController>();
+        pc.currentSpeed *= effect;
+    }
+
+    private void changeEnemyTime(GameObject enemy, float effect)
+    {
+        //TODO: change enemy time
+    }
+
+    private void changeBulletTime(GameObject bullet, float effect)
+    {
+        Bullet bulletController = bullet.GetComponent<Bullet>();
+        bulletController.movementSpeed *= effect;
+    }
+}
---
+++ Assets/Scripts/Cris Scripts/TimeManipulation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManipulation : MonoBehaviour{
+    public float timeSpeed = 1f;
+    private PlayerController pController;
+
+    private void Start()
+    {
+        pController = GetComponent<PlayerController>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            adjustSpeed();
+    }
+
+    private void adjustSpeed()
+    {
+        pController.currentSpeed = pController.originalSpeed * timeSpeed;
+    }
+
+}

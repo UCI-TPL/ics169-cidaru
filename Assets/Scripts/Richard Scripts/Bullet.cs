958ed5195c947e95df75e7bee0632ac4155bc6af
Rikerd <cursedmarkedsasuke@gmail.com>
2018-10-26 11:16:13 -0700

    Bullet to Center then Rotato
    Bullet to center then rotate

--- Assets/Scripts/Richard Scripts/Bullet.cs
+++ Assets/Scripts/Richard Scripts/Bullet.cs
@@ -11,17 +11,20 @@ public class Bullet : MonoBehaviour {
     }
 
     public float movementSpeed;
+    public float rotationSpeed = 200f;
 
     public int dmg = 1;
     private Vector3 center;
 
     private Rigidbody2D rb2d;
     private BulletMovementState currentMovement = BulletMovementState.Normal;
+    private float radius = 0f;
 
 	// Use this for initialization
 	void Awake () {
         rb2d = GetComponent<Rigidbody2D>();
         currentMovement = BulletMovementState.Normal;
+        radius = 0f;
     }
 	
 	// Update is called once per frame
@@ -38,16 +41,27 @@ public class Bullet : MonoBehaviour {
                 return;
             }
 
-            float step = movementSpeed * Time.deltaTime;
+            float step = (movementSpeed / 2) * Time.deltaTime;
             rb2d.position = Vector3.MoveTowards(transform.position, center, step);
+        } else if (currentMovement == BulletMovementState.Rotato)
+        {
+            float dist = Mathf.Abs(Vector3.Distance(transform.position, center));
+            if (dist <= 0.5f)
+            {
+                radius += 0.01f;
+            }
+
+            rb2d.MoveRotation(rb2d.rotation + rotationSpeed * Time.deltaTime);
+            rb2d.MovePosition(center + radius * new Vector3(Mathf.Cos(transform.rotation.eulerAngles.z * Mathf.PI / 180), Mathf.Sin(transform.rotation.eulerAngles.z * Mathf.PI / 180)));
         }
-        
+
     }
 
     public void startVortex(Vector3 c)
     {
         center = c;
         currentMovement = BulletMovementState.Succing;
+        transform.rotation = Quaternion.identity;
     }
 
     public void OnBecameInvisible()

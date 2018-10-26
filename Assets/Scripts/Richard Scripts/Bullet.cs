dee009d3334d3f192138a399eea610278ab52c3c
Rikerd <cursedmarkedsasuke@gmail.com>
2018-10-26 10:50:39 -0700

    Vortex Succ (No Rotato)
    Starting point for vortex succing
    Have not implemented rotato yet

--- Assets/Scripts/Richard Scripts/Bullet.cs
+++ Assets/Scripts/Richard Scripts/Bullet.cs
@@ -3,21 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour {
+    public enum BulletMovementState
+    {
+        Normal,
+        Succing,
+        Rotato
+    }
+
     public float movementSpeed;
 
     public int dmg = 1;
+    private Vector3 center;
 
     private Rigidbody2D rb2d;
+    private BulletMovementState currentMovement = BulletMovementState.Normal;
 
 	// Use this for initialization
 	void Awake () {
         rb2d = GetComponent<Rigidbody2D>();
-	}
+        currentMovement = BulletMovementState.Normal;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        Vector2 move = transform.up * movementSpeed * Time.deltaTime;
-        rb2d.MovePosition(rb2d.position + move);
+        if (currentMovement == BulletMovementState.Normal)
+        {
+            Vector2 move = transform.up * movementSpeed * Time.deltaTime;
+            rb2d.MovePosition(rb2d.position + move);
+        } else if (currentMovement == BulletMovementState.Succing)
+        {
+            if (transform.position == center)
+            {
+                currentMovement = BulletMovementState.Rotato;
+                return;
+            }
+
+            float step = movementSpeed * Time.deltaTime;
+            rb2d.position = Vector3.MoveTowards(transform.position, center, step);
+        }
+        
+    }
+
+    public void startVortex(Vector3 c)
+    {
+        center = c;
+        currentMovement = BulletMovementState.Succing;
     }
 
     public void OnBecameInvisible()
---
+++ Assets/Vortex.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vortex : MonoBehaviour {
+
+    public float setVortexTimer;
+
+    private float vortexTimer;
+
+    public enum VortexStates
+    {
+        Succ,
+        Blow
+    }
+
+    private VortexStates vortexState = VortexStates.Succ;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (vortexState == VortexStates.Succ)
+        {
+            col.GetComponent<Bullet>().startVortex(transform.position);
+        }
+    }
+
+
+}

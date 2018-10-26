022e63c60738a81f4686e9608d7af1bc8fa322c7
Rikerd <cursedmarkedsasuke@gmail.com>
2018-10-26 09:57:37 -0700

    Revert "Gun Controller"
    This reverts commit b14da85f77f39e0d60ac82d22a6193045faa3748.

--- Assets/Scripts/Richard Scripts/GunController.cs
+++ Assets/Scripts/Richard Scripts/GunController.cs
@@ -31,13 +31,7 @@ public class GunController : MonoBehaviour {
     // Takes mouse position and points gun towards that area
     private void FaceMouse()
     {
-        
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        //Vector3 result = ray.GetPoint((sr.transform.position.z - cam.transform.z) / ray.direction.z);
-
-        Vector3 mousePos = ray.GetPoint((0 - Camera.main.transform.position.z) / ray.direction.z);
-
-        mousePos.z = 0f;
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         gun.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
     }

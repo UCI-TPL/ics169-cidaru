3f52096baaf67361e443c42f0c299b539089cd3d
Crystal <crystal.agerton2015@gmail.com>
2018-10-30 20:53:39 -0700

    Added spawner delay

--- Assets/Scripts/Cris Scripts/EnemyControls/Spawner.cs
+++ Assets/Scripts/Cris Scripts/EnemyControls/Spawner.cs
@@ -2,27 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawner : MonoBehaviour {
+public class Spawner : MonoBehaviour
+{
 
     public GameObject thingToSpawn;
+    public int spawnDelay;
     public int timeInterval;
 
-    private float timer = 0;
+    private float totalTimer = 0;
+    private float timer;
+
+    private void Start()
+    {
+        timer = timeInterval;
+    }
 
 
     private void FixedUpdate()
     {
+        if (totalTimer < spawnDelay)
+        {
+            totalTimer += Time.deltaTime;
+            return;
+        }
+
         timer += Time.deltaTime;
         if (timer >= timeInterval)
         {
             SpawnThing();
             timer = 0;
         }
+
     }
 
     private void SpawnThing()
     {
         Instantiate(thingToSpawn, transform.position, Quaternion.identity);
     }
-
 }

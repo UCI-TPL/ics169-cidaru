323bd981b8e3feeccb0fecb5024247393e07682f
Isaac <isacmen@gmail.com>
2018-10-30 18:38:35 -0700

    animation stuff

---
+++ Assets/Scripts/Isaac Scripts/Walking.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walking : MonoBehaviour {
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = this.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("Horizontal") != 0 ||
+		    Input.GetAxis ("Vertical") != 0) {
+			anim.SetBool ("moving", true);
+		} else {
+			anim.SetBool ("moving", false);
+		}
+
+		if (Input.GetAxis ("Horizontal") < 0) {
+			this.transform.localScale = new Vector3 (-1.0f,
+				transform.localScale.y);
+		} else if(Input.GetAxis ("Horizontal") > 0) {
+			this.transform.localScale = new Vector3 (1.0f,
+				transform.localScale.y);
+		}
+	}
+}

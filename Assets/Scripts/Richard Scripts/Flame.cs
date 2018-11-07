201ef88dfdfeacfdf631182b2ca4fdfcb22bc4fa
Rikerd <cursedmarkedsasuke@gmail.com>
2018-11-07 12:14:25 -0800

    Burn Stasis
    Fire bullets burn target over time

--- Assets/Scripts/Richard Scripts/Flame.cs
+++ Assets/Scripts/Richard Scripts/Flame.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Flame : Projectile {
     public float setFlameLengthTimer = 1f;
 
+    public int tickAmount;
+    public float burnTimer;
+    public StasisController.Stasis stasisEffect;
+
     private float flameLengthTimer;
 
     public override void Awake()
@@ -31,4 +35,15 @@ public class Flame : Projectile {
             flameLengthTimer = setFlameLengthTimer;
         }
 	}
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            collision.GetComponent<StasisController>().setStasisProperties(stasisEffect, burnTimer, tickAmount);
+        } else if (collision.tag == "Obstacle")
+        {
+            Destroy(gameObject);
+        }
+    }
 }
---
+++ Assets/Scripts/Richard Scripts/StasisController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StasisController : MonoBehaviour {
+    public enum Stasis
+    {
+        Normal,
+        Burned
+    }
+    private float setStasisTickTimer;
+
+    private float stasisTickTimer;
+    private int tickAmount;
+    public Stasis currentStasis;
+
+    private Health hp;
+
+    private void Awake()
+    {
+        currentStasis = Stasis.Normal;
+
+        hp = GetComponent<Health>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (currentStasis == Stasis.Burned)
+        {
+            if (tickAmount != 0)
+            {
+                stasisTickTimer -= Time.deltaTime;
+
+                if (stasisTickTimer <= 0f)
+                {
+                    hp.TakeDamage(1);
+                    stasisTickTimer = setStasisTickTimer;
+                    tickAmount--;
+                }
+            } else
+            {
+                currentStasis = Stasis.Normal;
+            }
+        }
+	}
+
+    public void setStasisProperties(Stasis s, float stt, int tAmt) // stt = stasisTickTimer, tAmt = tickAmount
+    {
+        if (currentStasis != s)
+        {
+            setStasisTickTimer = stt;
+            stasisTickTimer = stt;
+        }
+
+        currentStasis = s;
+        tickAmount = tAmt;
+    }
+}

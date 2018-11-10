197be39b6809a014ddcbe1deffb932304c9907ec
Rikerd <cursedmarkedsasuke@gmail.com>
2018-11-10 14:24:59 -0800

    Door (WIP)
    Start of doors

---
+++ Assets/Scripts/Richard Scripts/Door.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+    public List<GameObject> enemies;
+
+    public Collider2D doorCollider;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!isCleared())
+        {
+            doorCollider.enabled = true;
+        }
+    }
+
+    public bool isCleared()
+    {
+        foreach (GameObject enemy in enemies)
+        {
+            return enemy == null;
+        }
+
+        return false;
+    }
+}

96a24c757575a10ef52d045d46a9b72ba7291327
Crystal <crystal.agerton2015@gmail.com>
2018-11-18 21:01:53 -0800

    Added Optional Raycast-based Regression

--- Assets/Scripts/Cris Scripts/EnemyControls/Enemy.cs
+++ Assets/Scripts/Cris Scripts/EnemyControls/Enemy.cs
@@ -14,6 +14,7 @@ public class Enemy : MonoBehaviour {
     public EnemyAttack attackStyle;
     public EnemyMovement movement;
     public int aggroRange;
+    public bool raycastEnabled;
 
     protected GameObject player;
     private Health hp;
@@ -37,16 +38,25 @@ public class Enemy : MonoBehaviour {
 	}
 	
 	void FixedUpdate () {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, player.transform.position - transform.position, Vector3.Distance(player.transform.position, transform.position));
-        if (hit && (hit.collider.tag == "Enemy Weapon" || hit.collider.tag == "Enemy"))
-            hit = Physics2D.Raycast(hit.collider.transform.position, player.transform.position - transform.position, Vector2.Distance(player.transform.position, transform.position));
-        //Debug.DrawLine(transform.position, player.transform.position, Color.red);
-        //Debug.DrawLine(transform.position, (Vector3)hit.point);
+        aggressing = true;
 
-        if (hit.collider == null)
-            aggressing = false;
-        else
-            aggressing = hit.collider.tag == "Player" && Vector3.Distance(player.transform.position, transform.position) <= aggroRange && currentState == EnemyState.Normal;
+        if (raycastEnabled)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, player.transform.position - transform.position, Vector3.Distance(player.transform.position, transform.position));
+            if (hit && (hit.collider.tag == "Enemy Weapon" || hit.collider.tag == "Enemy"))
+                hit = Physics2D.Raycast(hit.collider.transform.position, player.transform.position - transform.position, Vector2.Distance(player.transform.position, transform.position));
+
+            //Debug.DrawLine(transform.position, player.transform.position, Color.red);
+            //Debug.DrawLine(transform.position, (Vector3)hit.point);
+
+
+            if (hit.collider == null)
+                aggressing = false;
+            else
+                aggressing = hit.collider.tag == "Player";
+        }
+
+        aggressing = aggressing && Vector3.Distance(player.transform.position, transform.position) <= aggroRange && currentState == EnemyState.Normal;
 
         if (aggressing)
         {

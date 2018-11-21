c6c2529631d5ecb6f506ce1554c370e07480cbc6
Rikerd <cursedmarkedsasuke@gmail.com>
2018-11-21 13:51:34 -0800

    Spawners for Enemies

---
+++ Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour {
+
+    public GameObject enemy;
+
+    public void Awake()
+    {
+        Instantiate(enemy, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}

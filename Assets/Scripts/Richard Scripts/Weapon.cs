9913396e8e171917b77d1bd0690a7a4345ed54a0
Rikerd <cursedmarkedsasuke@gmail.com>
2018-11-28 01:59:55 -0800

    Weapon Revamp/Cleanup (WIP)
    Cleaning up weapon code to create proper ammo system

---
+++ Assets/Scripts/Richard Scripts/Weapon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Weapon", menuName = "Weapon")]
+public class Weapon : ScriptableObject
+{
+    [Header("Weapon Info")]
+    public string weaponName = "New Weapon";
+    public string weaponDescription = "Weapon Description";
+    public GameObject weaponBullet;
+
+    [Header("Weapon Properties")]
+    public int setMaxClipAmmo = 10;
+    public float setCooldown = 3f;
+
+    [HideInInspector]
+    public int currentClipAmmo;
+
+    [HideInInspector]
+    public int currentTotalAmmo;
+
+    public void Reload()
+    {
+
+    }
+
+    public bool CheckClip()
+    {
+        return (currentClipAmmo < setMaxClipAmmo && currentTotalAmmo > 0)
+    }
+}

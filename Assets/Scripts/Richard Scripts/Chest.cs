7b81fc679b028ed9135c88ed83367de39b13e119
Rikerd <cursedmarkedsasuke@gmail.com>
2018-11-27 20:26:34 -0800

    Sprint
    Fixed sprint

--- Assets/Scripts/Richard Scripts/Chest.cs
+++ Assets/Scripts/Richard Scripts/Chest.cs
@@ -36,15 +36,13 @@ public class Chest : MonoBehaviour {
 
     public virtual void giveAward(GameObject player)
     {
-        int rand = Random.Range(0, 3);
+        int rand = Random.Range(0, 2);
 
         if (rand == 0)
             player.GetComponent<BombController>().babyBomb.increaseMaxCharge();
         else if (rand == 1)
-            player.GetComponent<BombController>().hasteBomb.increaseMaxCharge();
-        else if (rand == 2)
             player.GetComponent<BombController>().slowBomb.increaseMaxCharge();
-        else if (rand == 3)
+        else if (rand == 2)
             player.GetComponent<GunController>().vortex.increaseMaxCharge();
     }
 }

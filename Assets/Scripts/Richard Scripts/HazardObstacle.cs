f038c0b3d08cfb5bc5962ad7fdf10efe71f0e714
Rikerd <cursedmarkedsasuke@gmail.com>
2018-11-28 11:55:11 -0800

    Hazardous Spike
    Deals dmg to player if touched

---
+++ Assets/Scripts/Richard Scripts/HazardObstacle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardObstacle : MonoBehaviour {
+
+    public int dmg = 1;
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<PlayerHealth>().TakeDamage(dmg);
+        }
+    }
+}

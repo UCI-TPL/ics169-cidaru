9aa35cd9c5b6935adc185a9536024a8741a8ba62
Dominic Langmesser <dlangmes@uci.edu>
2018-12-11 15:39:40 -0800

    updated the horse boss, no logic just fluffy stuff

---
+++ Assets/Scripts/Dom Scripts/HorseWheels.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(EnemyMovement))]
+public class HorseWheels : MonoBehaviour {
+    public int rotationSpeed = 10;
+    public GameObject leftWheel;
+    public GameObject rightWheel;
+    private Vector3 prevPos;
+    private float currVel;
+
+    private void Start()
+    {
+        prevPos = transform.position;
+
+    }
+
+    void FixedUpdate()
+    {
+        currVel = (prevPos - transform.position).magnitude / Time.deltaTime;
+        float rotSpeed = rotationSpeed  * currVel * Time.deltaTime;
+        leftWheel.transform.Rotate(new Vector3(0, 0, -1 * rotSpeed));
+        rightWheel.transform.Rotate(new Vector3(0, 0, -1 * rotSpeed));
+        prevPos = transform.position;
+    }
+}

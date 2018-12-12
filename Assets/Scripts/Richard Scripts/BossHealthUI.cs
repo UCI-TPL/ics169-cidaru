e0ffa99302f19e3c8e110d48e55c75a55b377975
Rikerd <cursedmarkedsasuke@gmail.com>
2018-12-11 19:39:41 -0800

    Boss Health Bar

---
+++ Assets/Scripts/Richard Scripts/BossHealthUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHealthUI : MonoBehaviour {
+    public Slider hpSlider;
+
+    private Health hp;
+
+	// Use this for initialization
+	void Awake () {
+        hp = GetComponent<Health>();
+        hpSlider.maxValue = hp.startingHealth;
+        hpSlider.value = hp.currentHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        hpSlider.value = hp.currentHealth;
+	}
+}

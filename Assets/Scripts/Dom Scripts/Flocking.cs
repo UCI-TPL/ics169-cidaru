01557ea104f3dfb71507433c9ab07b4e05f3df84
Dominic Langmesser <dlangmes@uci.edu>
2018-12-27 13:53:46 -0800

    whoops

--- Assets/Scripts/Dom Scripts/Flocking.cs
+++ Assets/Scripts/Dom Scripts/Flocking.cs
@@ -6,63 +6,65 @@ using UnityEngine;
 public class Flocking : MonoBehaviour {
 	private List<GameObject> enemies = new List<GameObject>();
 	private Rigidbody2D rb;
-	public Collider2D flockingRange;
-    float speed;
     Transform target;
     EnemyMovement mover;
 
+    public float rotSpeed = 100;
+    public float maxNeighborDist = 5;
+    public float speed;
 
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
         enemies = new List<GameObject>(GameObject.FindGameObjectsWithTag("Enemy"));
-        speed = Random.Range(3, 5);
+        speed = Random.Range(0.5f, 2);
         target = GameObject.FindGameObjectWithTag("Player").transform;
         mover = GetComponent<EnemyMovement>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        mover.Move(target.position);
+	void FixedUpdate () {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+        if (Random.Range(0, 6) < 1)
+            Flock();
+
 	}
 	
-	public Vector3 Separate(){
-		Vector3 v = new Vector3();
-		if(enemies.Count == 0)
-			return v;
-		foreach(GameObject go in enemies){
-			v.x += go.transform.position.x - transform.position.x;
-			v.y += go.transform.position.y - transform.position.y;
-		}
-		
-		v.x /= enemies.Count;
-		v.y /= enemies.Count;
-		v.x *= -1f;
-		v.y *= -1f;
-		Vector3.Normalize(v);
-		return v;
-	}
-	
-	
-	
-	// Keep list of all enemies inside circle collider trigger
-	void OnTriggerEnter2D(Collider2D other)
-	{
-		if(other.gameObject.tag == "Enemy"){
-			 if(!enemies.Contains(other.gameObject))
-			 {
-				 Debug.Log("New Enemy added" + enemies.Count);
-				 enemies.Add(other.gameObject);
-			 }
-		}
-	}
- 
-	void OnTriggerExit2D(Collider2D other)
-	{
-		 if(other.gameObject.tag == "Enemy"){
-			 if(enemies.Contains(other.gameObject))
-			 {
-				 enemies.Remove(other.gameObject);
-			 }
-		 }
-	}
+    void Flock()
+    {
+        Vector3 centerV = Vector3.zero;
+        Vector3 avoidV = Vector3.zero;
+
+        float dist;
+        int flockCount = 0;
+
+        foreach(GameObject go in enemies)
+        {
+            dist = Vector3.Distance(go.transform.position, this.transform.position);
+            if(dist <= maxNeighborDist)
+            {
+                centerV += go.transform.position;
+                flockCount++;
+
+                if (dist < 0.5f)
+                    avoidV = avoidV + (this.transform.position - go.transform.position);
+            }
+        }
+
+        if (flockCount > 0)
+        {
+            centerV = centerV / flockCount + (target.position - this.transform.position);
+
+            Vector3 direction = (centerV + avoidV) - transform.position;
+            if (direction != Vector3.zero)
+            {
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+                transform.rotation = Quaternion.Slerp(transform.rotation, q, rotSpeed * Time.deltaTime);
+
+            }
+        }
+        
+    }
 }

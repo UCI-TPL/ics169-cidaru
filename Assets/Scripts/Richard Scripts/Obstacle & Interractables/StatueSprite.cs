b1b2320577025ae322cd151771d9ec12c4714933
Rikerd <cursedmarkedsasuke@gmail.com>
2019-01-09 19:59:31 -0800

    Art Update
    Updated art for some objects
    Organized art folder

---
+++ Assets/Scripts/Richard Scripts/Obstacle & Interractables/StatueSprite.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatueSprite : MonoBehaviour {
+
+    public Sprite[] sprites;
+
+    private SpriteRenderer sr;
+
+	// Use this for initialization
+	void Start () {
+        sr = GetComponent<SpriteRenderer>();
+
+        int spriteIndex = Random.Range(0, sprites.Length);
+        sr.sprite = sprites[spriteIndex];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

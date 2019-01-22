6d706e844e397b903de8e1152ea08b91db2d9b83
Rikerd <cursedmarkedsasuke@gmail.com>
2019-01-21 16:46:24 -0800

    HP Pot Particles
    Added HP pot particles

--- Assets/Scripts/Richard Scripts/Obstacle & Interractables/HealthDrop.cs
+++ Assets/Scripts/Richard Scripts/Obstacle & Interractables/HealthDrop.cs
@@ -7,6 +7,8 @@ public class HealthDrop : MonoBehaviour {
     // Health amount to be restored to player
     public int healthRestore = 1;
 
+    public GameObject hpEffect;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // On player contact perform check if player is max health
@@ -16,6 +18,7 @@ public class HealthDrop : MonoBehaviour {
             if (!collision.GetComponent<PlayerHealth>().isMaxHealth())
             {
                 collision.GetComponent<PlayerHealth>().Heal(healthRestore);
+                Instantiate(hpEffect, transform.position, Quaternion.identity);
                 Destroy(gameObject);
             }
         }

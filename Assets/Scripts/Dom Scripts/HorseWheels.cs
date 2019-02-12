ce0e256a997318d510211f07a991cbff637d5de7
Dominic Langmesser <domlangmesser@aol.com>
2019-02-11 21:23:47 -0800

    boss scene stuffff

--- Assets/Scripts/Dom Scripts/HorseWheels.cs
+++ Assets/Scripts/Dom Scripts/HorseWheels.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 [RequireComponent(typeof(EnemyMovement))]
 public class HorseWheels : MonoBehaviour {
-    public int rotationSpeed = 10;
+    public int rotationSpeed = 100;
+    public int chargeRotSpeed = 500;
+
     public GameObject leftWheel;
     public GameObject rightWheel;
     private Vector3 prevPos;
     private float currVel;
+
+    bool charging = false;
 
     private void Start()
     {
@@ -19,9 +23,31 @@ public class HorseWheels : MonoBehaviour {
     void FixedUpdate()
     {
         currVel = (prevPos - transform.position).magnitude / Time.deltaTime;
-        float rotSpeed = rotationSpeed  * currVel * Time.deltaTime;
+        if (!charging)
+        {
+            float rotSpeed = rotationSpeed * currVel * Time.deltaTime;
+            wheelSpin(rotSpeed);
+        }
+        prevPos = transform.position;
+
+    }
+
+    void wheelSpin(float rotSpeed)
+    {
         leftWheel.transform.Rotate(new Vector3(0, 0, -1 * rotSpeed));
         rightWheel.transform.Rotate(new Vector3(0, 0, -1 * rotSpeed));
-        prevPos = transform.position;
+    }
+
+    public void Charge(float chargeDuration)
+    {
+        wheelSpin(chargeRotSpeed);
+        StartCoroutine(ChargeTimer(chargeDuration));
+    }
+
+    IEnumerator ChargeTimer(float chargeDuration)
+    {
+        charging = true;
+        yield return new WaitForSeconds(chargeDuration);
+        charging = false;
     }
 }

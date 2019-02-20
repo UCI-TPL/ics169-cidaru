5c8bee628de4df2eae9917906118fc21c190aae5
Rikerd <cursedmarkedsasuke@gmail.com>
2019-02-20 15:55:19 -0800

    Highlight Rooms Sprite Pt. 2

--- Assets/Scripts/Richard Scripts/Procedural Scripts/SpawnRoomTriggers.cs
+++ Assets/Scripts/Richard Scripts/Procedural Scripts/SpawnRoomTriggers.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnRoomTriggers : MonoBehaviour {
+    public HighlightRoom roomSprite;
+
+    private void Awake()
+    {
+        roomSprite.highlightRoomSprite();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
+            HighlightRoom[] roomSpriteControllers = FindObjectsOfType<HighlightRoom>();
+
+            foreach (HighlightRoom roomSpriteController in roomSpriteControllers)
+                roomSpriteController.resetRoom();
+
+            roomSprite.highlightRoomSprite();
+
             disableTurret();
             GameManager.gm.updateMinimapPosition(transform.parent.position);
         }

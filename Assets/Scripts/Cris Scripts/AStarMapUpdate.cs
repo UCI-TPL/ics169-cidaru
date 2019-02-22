41b26767e7711d7dd896b9716498cbd2edcc6c2a
Crystal <crystal.agerton2015@gmail.com>
2019-02-22 01:42:09 -0800

    Dumb boyz fix p1: the trials of troy

---
+++ Assets/Scripts/Cris Scripts/AStarMapUpdate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class AStarMapUpdate : MonoBehaviour
+{
+    /// <summary>
+    /// This is literally just to make sure the graphs update regularly
+    /// for the enemies. If this is not included, the penalties for where
+    /// the enemies are currently will not be properly updated.
+    /// </summary>
+    
+    private void FixedUpdate()
+    {
+        GetComponent<GraphUpdateScene>().Apply();
+    }
+}

839a82fc4e77741e94af9db773458d2c92f82afc
Crystal <crystal.agerton2015@gmail.com>
2019-02-23 17:26:36 -0800

    Ranged Enemy Movement
    has the basic stay-away now, not on prefabs yet

--- Assets/Scripts/Cris Scripts/EnemyControls/RangedMovement.cs
+++ Assets/Scripts/Cris Scripts/EnemyControls/RangedMovement.cs
@@ -6,16 +6,30 @@ public class RangedMovement : EnemyMovement {
     /*
      * Movement designed for the Ranged Enemies
      */
-
+    
     [Header("Ranged Movement")]
     public float minDistToPlayer; // This determines how close the enemy can get to the player before it stops
     public float margin = 0.1f; // Defines the range away from the min that's still allowed
+    public GameObject movableTarget;
+
+    protected override void setStartVars()
+    {
+        base.setStartVars();
+        movableTarget.transform.position = player.transform.position;
+        if (target == player.transform)
+        {
+            target = movableTarget.transform;
+        }
+    }
 
     public override void Move(bool aggressing)
     {
-        if (distFromPlayer() > minDistToPlayer + margin)
-            base.Move(aggressing);
-        else if (distFromPlayer() < minDistToPlayer - margin)
-            base.MoveAwayFrom(player.transform.position);
+        //if (distFromPlayer() > minDistToPlayer + margin)
+        //{
+        //    movableTarget.transform.position = (transform.position - player.transform.position).normalized * minDistToPlayer + player.transform.position;
+        //    target = movableTarget.transform;
+        //}
+        //else if (distFromPlayer() < minDistToPlayer - margin){}     
+        movableTarget.transform.position = (transform.position - player.transform.position).normalized * minDistToPlayer + player.transform.position;
     }
 }

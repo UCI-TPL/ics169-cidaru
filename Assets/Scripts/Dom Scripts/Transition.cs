ee84a70042eb5c5f25a8e1405e5a76376d7de6a7
Dominic Langmesser <domlangmesser@aol.com>
2019-03-02 13:40:37 -0800

    Transition Script

---
+++ Assets/Scripts/Dom Scripts/Transition.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transition : MonoBehaviour
+{
+    public Material transMat;
+
+    public Texture2D transitionTex; 
+    public float fadeTime = 1f;
+
+    [SerializeField]
+    private bool fade = false;
+    [SerializeField]
+    private bool unfade = false;
+    private float timer = 0;
+
+
+    void Start()
+    {
+        timer = 0;
+        transMat.SetFloat("_Cutoff", 0f);
+    }
+
+    public void Update()
+    {
+        if (fade && unfade)
+            fade = unfade = false;
+
+        transMat.SetTexture("_TransitionTex", transitionTex);
+        if (fade && transMat.GetFloat("_Cutoff") <= 0.9999)
+        {
+            print(timer);
+            timer += Time.deltaTime / fadeTime;
+            transMat.SetFloat("_Cutoff", Mathf.Lerp(0, 1, timer));
+        }
+        else if (!unfade)
+        {
+            fade = false;
+            timer = 0f;
+        }
+
+        if (unfade && transMat.GetFloat("_Cutoff") >= 0.001)
+        {
+            timer += Time.deltaTime / fadeTime;
+            transMat.SetFloat("_Cutoff", Mathf.Lerp(1, 0, timer));
+        }
+        else if (!fade)
+        {
+            unfade = false;
+            timer = 0f;
+        }
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        if (transMat != null)
+            Graphics.Blit(src, dst, transMat);
+    }
+
+    public void toBlack() { fade = true; }
+    public void fromBlack() { unfade = true; }
+
+}

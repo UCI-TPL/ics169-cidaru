c6ac80f91d137ebe1a4bfd946a15caea765effb7
Rikerd <cursedmarkedsasuke@gmail.com>
2019-03-05 21:15:14 -0800

    Intro Cutscene
    Dog Appears

---
+++ Assets/Scripts/Richard Scripts/Intro Cutscene Scripts/ClosePortal.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClosePortal : MonoBehaviour
+{
+    public Animator anim;
+
+    public void SetPortalClose()
+    {
+        anim.SetBool("close", true);
+    }
+}

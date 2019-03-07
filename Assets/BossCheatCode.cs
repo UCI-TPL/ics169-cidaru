f94b0fc9108399d4f3dbaa9181fe0a1a27ff5e72
Dominic Langmesser <domlangmesser@aol.com>
2019-03-06 21:13:41 -0800

    Tanenbaum playtest

---
+++ Assets/BossCheatCode.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BossCheatCode : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("X Button") && 
+            Input.GetButton("Y Button") && 
+            Input.GetButton("Left Bumper") && 
+            Input.GetButton("Right Bumper"))
+            {
+            SceneManager.LoadScene(5);
+        }
+
+    }
+}

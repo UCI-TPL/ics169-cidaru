b5eedd563e4809e44945722d229e3fa2d0ebcc68
Dominic Langmesser <domlangmesser@aol.com>
2019-03-06 19:06:03 -0800

    Start room changes (aesthetic)

--- Assets/Scripts/Richard Scripts/UI & Effects/BossHealthUI.cs
+++ Assets/Scripts/Richard Scripts/UI & Effects/BossHealthUI.cs
@@ -7,16 +7,25 @@ public class BossHealthUI : MonoBehaviour {
     public Slider hpSlider;
 
     private Health hp;
+    public GameObject winScreen;
 
 	// Use this for initialization
 	void Awake () {
         hp = GetComponent<Health>();
         hpSlider.maxValue = hp.startingHealth;
         hpSlider.value = hp.currentHealth;
+        winScreen.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
         hpSlider.value = hp.currentHealth;
+        if (hp.currentHealth <= 0)
+            win();
 	}
+
+    void win()
+    {
+        winScreen.SetActive(true);
+    }
 }
